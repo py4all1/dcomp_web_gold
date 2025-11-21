7d29f8ef0582f990a235dfe4a33e15482bd9f78f
py4all1 <nic.py4all@gmail.com>
2025-11-21 10:52:03 -0300

    atualização do sistem para produção

---
+++ emissor_gold/settings_prod.py
@@ -0,0 +1,36 @@
+# /srv/nfse/repo/emissor_gold/settings_prod.py
+from .settings import *  # noqa
+import os
+
+DEBUG = False
+SECRET_KEY = os.getenv("SECRET_KEY")  # vem do .env.prod
+ALLOWED_HOSTS = ["nfse.taxgold.com.br"]
+CSRF_TRUSTED_ORIGINS = ["https://nfse.taxgold.com.br"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "nfse_prod",
+        "USER": "nfse_owner",
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
+    }
+}
+
+# Sirva estáticos e mídia fora do repo
+STATIC_URL = "/static/"
+STATIC_ROOT = "/srv/nfse/staticfiles"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = "/srv/nfse/media"
+
+# HTTPS atrás do Nginx
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+# Hardening (ative assim que tudo ok)
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
